ed8f8bbe3e5b397caef9da90e3f1fcc3ac976201
yusukemurayama <yusuke@yusukemurayama.com>
2017-01-02 18:23:44 +0900

    UpperBreakoutIndicator, LowerBreakoutIndicatorを追加しました。

---
+++ ppyt/indicators/breakout_indicators.py
@@ -0,0 +1,53 @@
+# coding: utf-8
+import logging
+from ppyt import const
+from ppyt.indicators import IndicatorBase
+from ppyt.indicators.basic_indicators import (
+    PriceIndicator, RecentHighPriceIndicator, RecentLowPriceIndicator
+)
+
+logger = logging.getLogger(__name__)
+
+
+class UpperBreakoutIndicator(IndicatorBase):
+    """上にブレイクアウトしたかを示す指標です。"""
+    _findkey = 'UpperBreakout'
+
+    def _build_indicator(self, span, **kwds):
+        """indicatorのデータを組み立てます。
+
+        Args:
+            span: 過去何日間の高値を上に抜いたか
+        """
+        recent_indicator = RecentHighPriceIndicator(stock=self.stock,
+                                                    span=span)
+        price_indicator = PriceIndicator(stock=self.stock,
+                                         price_type=const.PRICE_TYPE_CLOSE)
+
+        arr1 = recent_indicator.shifted(-1)  # 昨日
+        arr2 = price_indicator.data  # 当日
+
+        # 昨日の終値が一昨日のX日間高値を上回っているかを判定します。
+        return arr1 < arr2
+
+
+class LowerBreakoutIndicator(IndicatorBase):
+    """下にブレイクアウトしたかを示す指標です。"""
+    _findkey = 'LowerBreakout'
+
+    def _build_indicator(self, span, **kwds):
+        """indicatorのデータを組み立てます。
+
+        Args:
+            span: 過去何日間の高値を上に抜いたか
+        """
+        recent_indicator = RecentLowPriceIndicator(stock=self.stock,
+                                                   span=span)
+        price_indicator = PriceIndicator(stock=self.stock,
+                                         price_type=const.PRICE_TYPE_CLOSE)
+
+        arr1 = recent_indicator.shifted(-1)  # 昨日
+        arr2 = price_indicator.data  # 当日
+
+        # 昨日の終値が一昨日のX日間安値を下回っているかを判定します。
+        return arr1 > arr2
